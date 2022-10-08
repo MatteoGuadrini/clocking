2a31efe6632668192c1c72d134f55977c3087c4f
Matteo Guadrini <matteo.guadrini@hotmail.it>
2022-10-08 10:39:41 +0200

    Add exec parts

--- clocking/cli.py
+++ clocking/cli.py
@@ -19,3 +19,12 @@
 #
 #     You should have received a copy of the GNU General Public License
 #     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+# region main
+if __name__ == '__main__':
+    # Check if database is created
+    # Check if configuration is created
+    # Check subcommand
+    pass
+
+# endregion
