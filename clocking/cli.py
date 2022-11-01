d6512be7852c54a7004417cadf2611fd76230eb7
Matteo Guadrini <matteo.guadrini@hotmail.it>
2022-11-01 09:35:36 +0100

    chore: add food ticket argument

--- clocking/cli.py
+++ clocking/cli.py
@@ -62,6 +62,7 @@ def get_args():
     set_group.add_argument('-H', '--holiday', help="Holiday value", default='Holiday', type=str)
     set_group.add_argument('-R', '--hour-reward', help="Hour reward", default=10.0, type=float)
     set_group.add_argument('-W', '--extraordinary-reward', help="Extraordinary hour reward", default=15.0, type=float)
+    set_group.add_argument('-F', '--food-ticket', help="Food ticket reward", default=7.0, type=float)
 
     return parser.parse_args()
 
