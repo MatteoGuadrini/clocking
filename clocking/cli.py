cdd70c5eaf5fc42cc9db949b30b5b01ed1db04e1
Matteo Guadrini <matteo.guadrini@hotmail.it>
2022-11-04 10:31:05 +0100

    chore: add disease argument

--- clocking/cli.py
+++ clocking/cli.py
@@ -59,6 +59,7 @@ def get_args():
                            type=str)
     set_group.add_argument('-E', '--extraordinary', help="Extraordinary hour value", default=1.0, type=float)
     set_group.add_argument('-P', '--permit-hour', help="Permit work hour value", default=1.0, type=float)
+    set_group.add_argument('-S', '--disease', help="Disease value", default='Disease', type=str)
     set_group.add_argument('-H', '--holiday', help="Holiday value", default='Holiday', type=str)
     set_group.add_argument('-C', '--currency', help="Currency value", default='$', type=str)
     set_group.add_argument('-R', '--hour-reward', help="Hour reward", default=10.0, type=float)
